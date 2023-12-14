40a6d8563b77a3c2f26a28079798ecd3c318e03b
Hesam Arabzadeh <40280976+hesam-a@users.noreply.github.com>
2023-12-14 10:55:42 -0600

    Add files via upload

---
+++ fix_stochastic_rotation.h
@@ -0,0 +1,52 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/ Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+
+#ifdef FIX_CLASS
+// clang-format off
+FixStyle(stochastic/rotation,FixStochasticRotation);
+// clang-format on
+#endif
+
+#ifndef LMP_FIX_STOCHASTIC_ROTATION_H
+#define LMP_FIX_STOCHASTIC_ROTATION_H
+
+#include <random>
+
+#include "fix.h"
+#include <Eigen/Dense>
+
+namespace LAMMPS_NS {
+
+class FixStochasticRotation : public Fix {
+ public:
+  FixStochasticRotation(class LAMMPS *, int, char **);
+  virtual ~FixStochasticRotation() {}
+  int setmask() override;
+//  void BuildRxMatrixEigen(Eigen::Matrix3d &, double);
+//  void BuildRyMatrixEigen(Eigen::Matrix3d &, double);
+//  void BuildRzMatrixEigen(Eigen::Matrix3d &, double);
+  std::pair<double,Eigen::Vector3d> calculateDominantInteractionVector(int , double **, int , int *); 
+  double scale_rotation(double, double );
+
+  void initial_integrate(int) override;
+
+ protected:
+  double angle_std_dev, coeff;
+  std::mt19937 gen;  // Random number generator
+
+};
+
+}
+
+#endif // LMP_FIX_STOCHASTIC_ROTATION_H
