82185faa4a54e354a54f16ada04ee5def28e4d1f
Hesam Arabzadeh <40280976+hesam-a@users.noreply.github.com>
2024-08-27 09:26:43 -0500

    Update pair_pauli_repuls.cpp

--- pair_pauli_repuls.cpp
+++ pair_pauli_repuls.cpp
@@ -25,6 +25,7 @@
 
 #include <cmath>
 #include <cstring>
+#include <iostream>
 
 using namespace LAMMPS_NS;
 
@@ -46,6 +47,7 @@ PairPauliRepuls::~PairPauliRepuls()
     memory->destroy(cutsq);
     memory->destroy(cut);
     memory->destroy(offset);
+    memory->destroy(overlap);
     memory->destroy(R);
     memory->destroy(amp);
     memory->destroy(decay_const);
@@ -57,7 +59,7 @@ PairPauliRepuls::~PairPauliRepuls()
 void PairPauliRepuls::compute(int eflag, int vflag) {
   int ii, i, j, jj, inum, jnum, itype, jtype;
   double xtmp, ytmp, ztmp, delx, dely, delz, evdwl, fpair;
-  double rsq, r, delta_r, pauli, factor_lj ;
+  double rsq, r, delta_r, over, pauli, factor_lj ;
   int *ilist, *jlist, *numneigh, **firstneigh;
 
   evdwl = 0.0;
@@ -96,13 +98,19 @@ void PairPauliRepuls::compute(int eflag, int vflag) {
       delz = ztmp - x[j][2];
       rsq = delx*delx + dely*dely + delz*delz;
       jtype = type[j];
-      r = sqrt(rsq);
+
 
       if (rsq < cutsq[itype][jtype]) {
-        delta_r = r - (0.4 * R[itype][jtype]);
+        //std::cout << " PAULI : if loop was entered\n";
+        //std::cout << "\nInside Pauli/Repuls\n   Particle i: type " << itype << ", global index " << atom->tag[i]     
+        //        << ")      Particle j: type: " << jtype << ", global index " << atom->tag[j] << ")\n"; 
+
+        r = sqrt(rsq);
+	over = overlap[itype][jtype] * R[itype][jtype];
+        delta_r = r - over;
 
         pauli = amp[itype][jtype] * exp(-decay_const[itype][jtype] * delta_r);
-	fpair = decay_const[itype][jtype] * pauli;
+        fpair = decay_const[itype][jtype] * pauli;
 
         // Apply the special scaling factor to the forces
         fpair *= factor_lj / r;
@@ -141,6 +149,7 @@ void PairPauliRepuls::allocate()
   memory->create(cutsq, np1, np1, "pair:cutsq");
   memory->create(cut, np1, np1, "pair:cut");
   memory->create(offset, np1, np1, "pair:offset");
+  memory->create(overlap, np1, np1, "pair:overlap");
   memory->create(R, np1, np1, "pair:R");
   memory->create(amp, np1, np1, "pair:amp");
   memory->create(decay_const, np1, np1, "pair:decay_const");
@@ -170,25 +179,36 @@ void PairPauliRepuls::settings(int narg, char **arg)
 
 void PairPauliRepuls::coeff(int narg, char **arg)
 {
-  if (narg != 9) error->all(FLERR, "Incorrect args for pair coefficients");
+  if (narg < 9 || narg > 10) error->all(FLERR, "Incorrect args for pair coefficients");
   if (!allocated) allocate();
 
   int ilo, ihi, jlo, jhi;
   utils::bounds(FLERR, arg[0], 1, atom->ntypes, ilo, ihi, error);
   utils::bounds(FLERR, arg[1], 1, atom->ntypes, jlo, jhi, error);
 
-  double R_i     = utils::numeric(FLERR, arg[2], false, lmp);
-  double amp_i   = utils::numeric(FLERR, arg[3], false, lmp);
-  double decay_const_i = utils::numeric(FLERR, arg[4], false, lmp);
-  double R_j     = utils::numeric(FLERR, arg[5], false, lmp);
-  double amp_j   = utils::numeric(FLERR, arg[6], false, lmp);
-  double decay_const_j = utils::numeric(FLERR, arg[7], false, lmp);
-  double cut_one = utils::numeric(FLERR, arg[8], false, lmp);
+//  // Check if the 'none' keyword is used
+//  if (strcmp(arg[2], "none") == 0) {
+//    for (int i = ilo; i <= ihi; i++) {
+//      for (int j = MAX(jlo, i); j <= jhi; j++) {
+//        setflag[i][j] = 0; 
+//      }
+//    }
+//    return; // Exit the function
+//  }  
+
+  double overlap = utils::numeric(FLERR, arg[2], false, lmp);
+  double R_i     = utils::numeric(FLERR, arg[3], false, lmp);
+  double amp_i   = utils::numeric(FLERR, arg[4], false, lmp);
+  double decay_const_i = utils::numeric(FLERR, arg[5], false, lmp);
+  double R_j     = utils::numeric(FLERR, arg[6], false, lmp);
+  double amp_j   = utils::numeric(FLERR, arg[7], false, lmp);
+  double decay_const_j = utils::numeric(FLERR, arg[8], false, lmp);
+  double cut_one = utils::numeric(FLERR, arg[9], false, lmp);
 
   int count = 0;
   for (int i = ilo; i <= ihi; i++) {
     for (int j = MAX(jlo, i); j <= jhi; j++) {
-      R[i][j] = 0.5 * (R_i + R_j);        // Using arithmetic mean for mixing
+      R[i][j] = (R_i + R_j);        // Using arithmetic mean for mixing
       amp[i][j] = sqrt(amp_i * amp_j);    // Using geometric mean for mixing
       decay_const[i][j] = 0.5 * (decay_const_i + decay_const_j);  // Using arithmetic mean for mixing
       cut[i][j] = cut_one;
@@ -204,19 +224,21 @@ void PairPauliRepuls::coeff(int narg, char **arg)
    init for one type pair i,j and corresponding j,i
 ------------------------------------------------------------------------- */
 
-double PairPauliRepuls::init_one(int i, int j)
-{
+double PairPauliRepuls::init_one(int i, int j) {
   if (setflag[i][j] == 0) error->all(FLERR, "All pair coeffs are not set");
 
-  // Compute the offset
-  offset[i][j] = amp[i][j] *  exp(-decay_const[i][j] * cut[i][j]);
+  if (offset_flag) {
+    offset[i][j] = amp[i][j] * exp(-decay_const[i][j] * (cut[i][j] - (overlap[i][j] * R[i][j])));
+  } else {
+    offset[i][j] = 0.0;
+  }
 
   // Symmetrize the potential parameter arrays
-  cut[j][i]    = cut[i][j];
-  amp[j][i]     = amp[i][j];
-  decay_const[j][i]   = decay_const[i][j];
+  cut[j][i] = cut[i][j];
+  amp[j][i] = amp[i][j];
+  decay_const[j][i] = decay_const[i][j];
   offset[j][i] = offset[i][j];
-
+  overlap[j][i] = overlap[i][j];
 
   return cut[i][j];
 }
@@ -226,14 +248,16 @@ double PairPauliRepuls::init_one(int i, int j)
 double PairPauliRepuls::single(int /*i*/, int /*j*/, int itype, int jtype, double rsq,
                              double /*factor_coul*/, double factor_lj, double &fforce)
 {
-  double r, delta_r, pauli;
+  double r, delta_r, pauli, fpair, over;
 
   r = sqrt(rsq);
-  delta_r = r - R[itype][jtype]; 
+  over = overlap[itype][jtype] * R[itype][jtype];
+  delta_r = r - over;
 
   pauli = amp[itype][jtype] * exp(-decay_const[itype][jtype] * delta_r);
+  fpair = decay_const[itype][jtype] * pauli;
 
-  fforce = factor_lj * -decay_const[itype][jtype] * pauli/r;
+  fforce = factor_lj * fpair/r;
   return factor_lj * (pauli - offset[itype][jtype]);
 }
 
@@ -251,6 +275,7 @@ void PairPauliRepuls::write_restart(FILE *fp)
       fwrite(&setflag[i][j], sizeof(int), 1, fp);
       if (setflag[i][j]) {
         fwrite(&decay_const[i][j], sizeof(double), 1, fp);
+        fwrite(&overlap[i][j], sizeof(double), 1, fp);
         fwrite(&cut[i][j], sizeof(double), 1, fp);
         fwrite(&amp[i][j], sizeof(double), 1, fp);
         fwrite(&R[i][j],   sizeof(double), 1, fp);
@@ -289,11 +314,13 @@ void PairPauliRepuls::read_restart(FILE *fp)
       if (setflag[i][j]) {
         if (me == 0) {
           utils::sfread(FLERR, &decay_const[i][j], sizeof(double), 1, fp, nullptr, error);
+          utils::sfread(FLERR, &overlap[i][j], sizeof(double), 1, fp, nullptr, error);
           utils::sfread(FLERR, &cut[i][j], sizeof(double), 1, fp, nullptr, error);
           utils::sfread(FLERR, &amp[i][j], sizeof(double), 1, fp, nullptr, error);
           utils::sfread(FLERR, &R[i][j], sizeof(double), 1, fp, nullptr, error);
         }
         MPI_Bcast(&decay_const[i][j], 1, MPI_DOUBLE, 0, world);
+        MPI_Bcast(&overlap[i][j], 1, MPI_DOUBLE, 0, world);
         MPI_Bcast(&cut[i][j], 1, MPI_DOUBLE, 0, world);
         MPI_Bcast(&amp[i][j], 1, MPI_DOUBLE, 0, world);
         MPI_Bcast(&R[i][j], 1, MPI_DOUBLE, 0, world);
@@ -326,7 +353,7 @@ void PairPauliRepuls::write_data(FILE *fp)
 {
   for (int i = 1; i <= atom->ntypes; i++)
     for (int j = i; j <= atom->ntypes; j++)
-      fprintf(fp, "%d %d %g %g %g %g\n", i, j, decay_const[i][j], cut[i][i], amp[i][j],  R[i][i]);
+      fprintf(fp, "%d %d %g %g %g %g %g\n", i, j, decay_const[i][j], overlap[i][j], cut[i][i], amp[i][j],  R[i][i]);
 }
 
 /* ----------------------------------------------------------------------
@@ -337,7 +364,7 @@ void PairPauliRepuls::write_data_all(FILE *fp)
 {
   for (int i = 1; i <= atom->ntypes; i++)
     for (int j = i; j <= atom->ntypes; j++)
-      fprintf(fp, "%d %d %g %g %g %g\n", i, j, decay_const[i][j], cut[i][j], amp[i][j], R[i][i]);
+      fprintf(fp, "%d %d %g %g %g %g %g\n", i, j, decay_const[i][j], overlap[i][j], cut[i][j], amp[i][j], R[i][i]);
 }
 
 
@@ -347,6 +374,7 @@ void *PairPauliRepuls::extract(const char *str, int &dim)
 {
   dim =2;
   if (strcmp(str, "decay_const") == 0) return (void *) decay_const;
+  if (strcmp(str, "overlap") == 0) return (void *) overlap;
   if (strcmp(str, "amp" )  == 0)  return (void *) amp;
   if (strcmp(str, "R" )  == 0)  return (void *) R;
   return nullptr;
